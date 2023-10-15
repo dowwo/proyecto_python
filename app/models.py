bf2200628185ec05661332d4e66b1efa64cd912f
dowwo <roggers.morales@gmail.com>
2023-10-15 18:49:22 -0300

    Clase User
    Agregada una clase User y su relación con la clase Tarea

--- app/models.py
+++ app/models.py
@@ -3,6 +3,15 @@ from sqlalchemy.orm import relationship
 
 from .database import Base
 
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String) # Todavia no se si hacer el hash
+    is_active = Column(Boolean, default=True)
+
+    tareas = relationship("Tarea", back_populates="owner")
 
 class Tarea(Base):
     __tablename__ = "tareas"
@@ -12,6 +21,8 @@ class Tarea(Base):
     descripcion = Column(String, index=True)
     fecha_vencimiento = Column(Date, index=True)
     activa = Column(Boolean, default=True)
+
+    owner = relationship("User", back_populates="tareas")
 
 
 
