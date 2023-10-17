941426f66c6510959466af82b57e9d40791d887d
dowwo <roggers.morales@gmail.com>
2023-10-17 17:16:03 -0300

    Modificación del archivo crud
    Creadas las funciones para Tarea y User
    Funciones para User:
    - obtener usuario
    - obtener usuario por email
    - obtener usuarios
    - crear usuario
    - eliminar usuario
    
    -
    Funciones de Tarea:
    - crear tarea
    - obtener tareas
    - obtener tareas por id de usuario
     - eliminar tarea por id(no implementada)
    - actualizar tarea (no implementada)

---
+++ app/crud.py
@@ -0,0 +1,74 @@
+# Importamos Session de SQLAlchemy para realizar las solicitudes a la base de datos
+from sqlalchemy.orm import Session
+
+# Importamos models y shcemas desde la misma carpeta
+from . import models, schemas
+# Función que devuelve un usuario por su ID
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+# Función que devuelve un usuario por su email
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+# 
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+# Función para crear un usuario
+def create_user(db: Session, user: schemas.UserCreate):
+    fake_hashed_password = user.password
+    db_user = models.User(email= user.email, hashed_password=fake_hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+# Función para eliminar un usuario por su ID
+def delete_user_by_id(db: Session, user_id: int):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    # Si no existe
+    if user is None:
+        return None
+    # Si existe
+    else:
+        db.delete(user)
+        db.commit()
+        return user
+
+# Obtener tarea por id
+def get_tarea(db: Session, user_id: int, activa: bool, skip: int = 0, limit: int = 100):
+    return db.query(models.Tarea).filter(models.Tarea.owner_id == user_id, models.Tarea.activa == activa).offset(skip).limit(limit).first()
+
+
+# Función que devuelve una lista de tareas
+def get_tareas(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Tarea).offset(skip).limit(limit).all()
+
+# Función para crear una nueva tarea a un usuario
+def create_user_tarea(db: Session, tarea: schemas.TareaCreate, user_id: int):
+    db_tarea = models.Tarea(**tarea.dict(), owner_id=user_id)
+    db.add(db_tarea)
+    db.commit()
+    db.refresh(db_tarea)
+    return db_tarea
+# Función para eliminar una tarea a un usuario
+# POR PROBAR ---------------------------
+def delete_user_tarea(db: Session, tarea_id: int, user_id: int):
+    db_tarea = db.query(models.Tarea).filter(models.Tarea.id == tarea_id, models.Tarea.owner_id == user_id).first()
+    if db_tarea is None:
+        return None
+    db.delete(db_tarea)
+    db.commit()
+    return db_tarea
+# Función para actualizar una tarea de un usuario
+# POR PROBAR -----------------------------
+def update_user_tarea(db: Session, tarea_id: int, user_id: int, tarea: schemas.TareaCreate):
+    db_tarea = db.query(models.Tarea).filter(models.Tarea.id == tarea_id, models.Tarea.owner_id == user_id).first()
+    if db_tarea is None:
+        return None
+    for key, value in tarea.dict().items():
+        setattr(db_tarea, key, value)
+    db.commit()
+    db.refresh(db_tarea)
+    return db_tarea
