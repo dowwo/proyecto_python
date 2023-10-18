d02387b2b698915f91c63e470b1f5c54198746ae
dowwo <roggers.morales@gmail.com>
2023-10-18 13:58:08 -0300

    Modificación de endpoint para tareas
    Comentada la forma anterior donde se realiza la consulta desde el main
    
    Se cambió en la función read_tareas:
    - variable db_tarea que le pasa el user_id y activa en True o False
    Ahora la respuesta viene desde el método en el archivo crud.py
    
    Se agregó un endpoint para modificar el estado de la tarea con la ruta @app.put("/tareas/{tarea_id}")

--- app/main.py
+++ app/main.py
@@ -48,7 +48,7 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
 
 # Respuesta a la petición para modificar un usuario
 
-# Respuesta a la solicitud DELETE para eliminar un usuario pasando su id en la URL
+# endpoint DELETE para eliminar un usuario pasando su id en la URL
 @app.delete("/users/{user_id}", response_model=schemas.UserDelete)
 # Se pasa el id del usuario en la URL
 def delete_user(user_id: int, db: Session = Depends(get_db)):
@@ -76,43 +76,30 @@ def read_tareas(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     tareas = crud.get_tareas(db, skip=skip, limit=limit)
     return tareas
 
-# Este bloque de código NO funciona
-# @app.get("/tareas/{user_id}", response_model=schemas.Tarea)
-# def read_tareas_by_user(user_id: int, skip: int = 0, limit: int = 100, activa: bool = None, db: Session = Depends(get_db)):
-#     if activa is False: # False
-#         tareas = crud.get_tarea(db, user_id=user_id, activa=False, skip=skip, limit=limit)
-#     else: # True
-#         tareas = crud.get_tarea(db, user_id=user_id, activa=True, skip=skip, limit=limit)  
-#     if not tareas:
-#         raise HTTPException(status_code=404, detail="No existen tareas para este usuario")
-       
-#     return tareas
+# Respuesta a solicitud GET para mostrar tareas por True o False
+# ESTA ES LA ANTERIOR
+# @app.get("/tareas/{user_id}")
+# async def read_tareas(user_id: int, activa: bool = False, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+#     if activa:
+#         # Cambiar esto para que la respuesta la retorne desde el crud
+#         return db.query(models.Tarea).filter(models.Tarea.activa == True, models.Tarea.owner_id == user_id).offset(skip).limit(limit).all()
+#     else:
+#         return db.query(models.Tarea).filter(models.Tarea.activa == False, models.Tarea.owner_id == user_id).offset(skip).limit(limit).all()
 
-# @app.get("/tareas/{user_id}", response_model=schemas.Tarea)
-# def read_tareas_by_user(user_id: int, activa: bool = False, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-#     if activa is False: # False
-#         tareas = crud.get_tarea(db, user_id=user_id, activa=False, skip=skip, limit=limit)
-#     else: # True
-#         tareas = crud.get_tarea(db, user_id=user_id, activa=True, skip=skip, limit=limit)  
-#     if not tareas:
-#         raise HTTPException(status_code=404, detail="No existen tareas para este usuario")
-       
-#     return tareas
-
-# @app.get("/tareas/{user_id}", response_model=List[schemas.Tarea])
-# def read_tareas(user_id: int, activa: bool, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    
-#     tareas = crud.get_tareas(db, skip=skip, limit=limit)
-#     return tareas
-
-
+# Respuesta a solicitud GET para mostrar tareas por True o False
 @app.get("/tareas/{user_id}")
-async def read_tareas(user_id: int, activa: bool = False, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    if activa:
-        return db.query(models.Tarea).filter(models.Tarea.activa == True, models.Tarea.owner_id == user_id).offset(skip).limit(limit).all()
-    else:
-        return db.query(models.Tarea).filter(models.Tarea.activa == False, models.Tarea.owner_id == user_id).offset(skip).limit(limit).all()
+def read_tareas(user_id: int, activa: bool = False,db: Session = Depends(get_db)):
+    db_tarea = crud.get_tareas(db, user_id, activa)
+    if db_tarea is None:
+        return HTTPException(status_code=404, detail="Tarea no encontrada")
+    return db_tarea
 
+@app.put("/tareas/{tarea_id}", response_model=schemas.Tarea)
+def update_tarea(tarea_id: int, tarea_update: schemas.TareaUpdate, db: Session = Depends(get_db)):
+    db_tarea = crud.update_user_tarea_estado(db, tarea_id, tarea_update)
+    if db_tarea is None:
+        raise HTTPException(status_code=404, detail="Tarea no encontrada")
+    return db_tarea
 
         
 
