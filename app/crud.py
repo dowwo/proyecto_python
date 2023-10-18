2e58ae19ef8b208308527d97397002545672352c
dowwo <roggers.morales@gmail.com>
2023-10-18 15:49:36 -0300

    Update crud.py
    Se cambió la forma en que se pretendía modificar la información del usuario, ahora ya funciona
    - Agregada una función update_user para realizar cambios al schema del UserUpdate que hereda los atributos que se usan en UserBase
    
    Agregada una función para eliminar una tarea que recibe el id, filtra y elimina usando el id de la tarea

--- app/crud.py
+++ app/crud.py
@@ -27,6 +27,17 @@ def create_user(db: Session, user: schemas.UserCreate):
         print ("Escepción?: ", e)
     finally:
         return db_user
+    
+#actualizar/modificar usuario
+def update_user(db: Session, user_id: int, user_update: schemas.UserUpdate):
+    db_user = db.query(models.User).filter(models.User.id == user_id).first()
+    if db_user is None:
+        return None
+    for field, value in user_update.dict().items():
+        setattr(db_user, field, value)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
 
 # Función para eliminar un usuario por su ID
 def delete_user_by_id(db: Session, user_id: int):
@@ -44,6 +55,9 @@ def delete_user_by_id(db: Session, user_id: int):
 def get_tarea(db: Session, user_id: int, activa: bool, skip: int = 0, limit: int = 100):
     return db.query(models.Tarea).filter(models.Tarea.owner_id == user_id).offset(skip).limit(limit).first()
 
+def get_tarea_by_id(db: Session, tarea_id: int, skip: int = 0, limit: int = 100):
+    return db.query(models.Tarea).filter(models.Tarea.id == tarea_id).offset(skip).limit(limit).first()
+
 
 # Función que devuelve una lista de tareas
 def get_tareas(db: Session, user_id: int, activa: bool, skip: int = 0, limit: int = 100):
@@ -59,29 +73,9 @@ def create_user_tarea(db: Session, tarea: schemas.TareaCreate, user_id: int):
     db.commit()
     db.refresh(db_tarea)
     return db_tarea
-# Función para eliminar una tarea a un usuario
-# POR PROBAR ---------------------------
-def delete_user_tarea(db: Session, tarea_id: int, user_id: int):
-    db_tarea = db.query(models.Tarea).filter(models.Tarea.id == tarea_id, models.Tarea.owner_id == user_id).first()
-    if db_tarea is None:
-        return None
-    db.delete(db_tarea)
-    db.commit()
-    return db_tarea
-# Función para actualizar una tarea de un usuario
-# POR PROBAR -----------------------------
-def update_user_tarea(db: Session, tarea_id: int, user_id: int, tarea: schemas.TareaCreate):
-    db_tarea = db.query(models.Tarea).filter(models.Tarea.id == tarea_id, models.Tarea.owner_id == user_id).first()
-    if db_tarea is None:
-        return None
-    for key, value in tarea.dict().items():
-        setattr(db_tarea, key, value)
-    db.commit()
-    db.refresh(db_tarea)
-    return db_tarea
 
-#actualizar datos de tarea 
-def update_user_tarea_estado(db: Session, tarea_id: int, tarea_update: schemas.TareaUpdate):
+# Función para actualizar una tarea de un usuario
+def update_user_tarea(db: Session, tarea_id: int, tarea_update: schemas.TareaBase):
     db_tarea = db.query(models.Tarea).filter(models.Tarea.id == tarea_id).first()
     if db_tarea is None:
         return None
@@ -90,3 +84,26 @@ def update_user_tarea_estado(db: Session, tarea_id: int, tarea_update: schemas.T
     db.commit()
     db.refresh(db_tarea)
     return db_tarea
+
+#actualizar estado de una tarea
+def update_user_tarea_estado(db: Session, tarea_id: int, tarea_update: schemas.TareaUpdateEstado):
+    db_tarea = db.query(models.Tarea).filter(models.Tarea.id == tarea_id).first()
+    if db_tarea is None:
+        return None
+    for field, value in tarea_update.dict().items():
+        setattr(db_tarea, field, value)
+    db.commit()
+    db.refresh(db_tarea)
+    return db_tarea
+
+# Función para eliminar una tarea por su ID
+def delete_tarea_by_id(db: Session, tarea_id: int):
+    tarea = db.query(models.Tarea).filter(models.Tarea.id == tarea_id).first()
+    # Si no existe
+    if tarea is None:
+        return None
+    # Si existe
+    else:
+        db.delete(tarea)
+        db.commit()
+        return tarea
