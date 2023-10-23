6e73de5dd065c711c663e22f9ee711fbeedda320
dowwo <roggers.morales@gmail.com>
2023-10-23 17:17:46 -0300

    Update crud.py
    Modificadas las funciones para create_user, se quitó el try except, ya que no era posible hacer pasar las pruebas
    
    Eliminada la condición if else de la función delete_user_by_id
    
    Se cambió user_update.dict() por user_update.model_dump() para evitar avisos de obsolencia en las pruebas unitarias
    
    Se cambió update_user_tarea.dict() por update_user_tarea.model_dump() para evitar avisos de obsolencia en las pruebas unitarias
    
    Eliminada la condición if else de la función delete_user_tarea

--- app/crud.py
+++ app/crud.py
@@ -17,23 +17,21 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
 
 # Función para crear un usuario
 def create_user(db: Session, user: schemas.UserCreate):
-    try:
-        fake_hashed_password = user.password
-        db_user = models.User(email= user.email, hashed_password=fake_hashed_password)
-        db.add(db_user)
-        db.commit()
-        db.refresh(db_user)
-    except Exception as e:
-        print ("Escepción?: ", e)
-    finally:
-        return db_user
+
+    fake_hashed_password = user.password
+    db_user = models.User(email= user.email, hashed_password=fake_hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    
+    return db_user
     
 #actualizar/modificar usuario
 def update_user(db: Session, user_id: int, user_update: schemas.UserUpdate):
     db_user = db.query(models.User).filter(models.User.id == user_id).first()
     if db_user is None:
         return None
-    for field, value in user_update.dict().items():
+    for field, value in user_update.model_dump().items():
         setattr(db_user, field, value)
     db.commit()
     db.refresh(db_user)
@@ -42,18 +40,9 @@ def update_user(db: Session, user_id: int, user_update: schemas.UserUpdate):
 # Función para eliminar un usuario por su ID
 def delete_user_by_id(db: Session, user_id: int):
     user = db.query(models.User).filter(models.User.id == user_id).first()
-    # Si no existe
-    if user is None:
-        return None
-    # Si existe
-    else:
-        db.delete(user)
-        db.commit()
-        return user
-
-# Obtener tarea por id
-def get_tarea(db: Session, user_id: int, activa: bool, skip: int = 0, limit: int = 100):
-    return db.query(models.Tarea).filter(models.Tarea.owner_id == user_id).offset(skip).limit(limit).first()
+    db.delete(user)
+    db.commit()
+    return user
 
 def get_tarea_by_id(db: Session, tarea_id: int, skip: int = 0, limit: int = 100):
     return db.query(models.Tarea).filter(models.Tarea.id == tarea_id).offset(skip).limit(limit).first()
@@ -66,13 +55,13 @@ def get_tareas(db: Session, user_id: int, activa: bool, skip: int = 0, limit: in
     else:
         return db.query(models.Tarea).filter(models.Tarea.activa == False, models.Tarea.owner_id == user_id).offset(skip).limit(limit).all()
 
-# Función para crear una nueva tarea a un usuario
-def create_user_tarea(db: Session, tarea: schemas.TareaCreate, user_id: int):
-    db_tarea = models.Tarea(**tarea.model_dump(), owner_id=user_id)
-    db.add(db_tarea)
-    db.commit()
-    db.refresh(db_tarea)
-    return db_tarea
+# # Función para crear una nueva tarea a un usuario
+# def create_user_tarea(db: Session, tarea: schemas.TareaCreate, user_id: int):
+#     db_tarea = models.Tarea(**tarea.model_dump(), owner_id=user_id)
+#     db.add(db_tarea)
+#     db.commit()
+#     db.refresh(db_tarea)
+#     return db_tarea
 
 # Función para actualizar una tarea de un usuario
 def update_user_tarea(db: Session, tarea_id: int, tarea_update: schemas.TareaBase):
@@ -90,7 +79,7 @@ def update_user_tarea_estado(db: Session, tarea_id: int, tarea_update: schemas.T
     db_tarea = db.query(models.Tarea).filter(models.Tarea.id == tarea_id).first()
     if db_tarea is None:
         return None
-    for field, value in tarea_update.dict().items():
+    for field, value in tarea_update.model_dump().items():
         setattr(db_tarea, field, value)
     db.commit()
     db.refresh(db_tarea)
@@ -99,11 +88,6 @@ def update_user_tarea_estado(db: Session, tarea_id: int, tarea_update: schemas.T
 # Función para eliminar una tarea por su ID
 def delete_tarea_by_id(db: Session, tarea_id: int):
     tarea = db.query(models.Tarea).filter(models.Tarea.id == tarea_id).first()
-    # Si no existe
-    if tarea is None:
-        return None
-    # Si existe
-    else:
-        db.delete(tarea)
-        db.commit()
-        return tarea
+    db.delete(tarea)
+    db.commit()
+    return tarea
